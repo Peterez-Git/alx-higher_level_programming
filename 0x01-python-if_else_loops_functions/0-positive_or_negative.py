a2112f14b1999050b2597510e8d07136eb95599b
Peter Anthony <peterez5876@gmail.com>
2024-01-02 15:56:25 -0800

    Using IF statements and the random module

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-5, 5)
+if number > 0:
+    print('The number is positive')
+elif number == 0:
+    print('The number is zero')
+else:
+    print('The number is negative')
