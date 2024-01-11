a92efecd3f1c224cf2c2354d34409505cf88a172
Peter Anthony <peterez5876@gmail.com>
2024-01-11 05:58:41 -0800

    Updated from exponent to times two

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 def multiply_by_2(a_dictionary):
     squared_dict = {}
     for key, values in a_dictionary.items():
-        squared_dict[key] = values ** 2
+        squared_dict[key] = values * 2
     return squared_dict
