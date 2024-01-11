3e45e67ccbd54ce44c69b9854f8fd8c932b3e3c3
Peter Anthony <peterez5876@gmail.com>
2024-01-10 18:16:41 -0800

    Sorting a Dictionary by it's keys

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    sort_keys = sorted(a_dictionary.keys())
-    sorted_dict = {key: a_dictionary[key] for key in sort_keys}
-    print(sorted_dict)
+    for key in sorted(a_dictionary.keys()):
+        print(f"{key}: {a_dictionary[key]}")
