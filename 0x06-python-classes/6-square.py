32c43792b7060d320bf06fb25b9a733ff4304cf7
Peter Anthony <peterez5876@gmail.com>
2024-01-23 15:25:53 -0800

    Updated

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -31,9 +31,15 @@ class Square:
 
     @property
     def position(self):
+        """Get/set the current position of the square."""
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
                 not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
             raise TypeError("postion must be a tuple of 2 postive integers")
         self.__position = value
 
