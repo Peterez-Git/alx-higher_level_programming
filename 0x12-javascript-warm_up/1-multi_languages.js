27fb2e8e416867524fc9e3eb2064ba847de9f7fb
Peter Anthony <peterez5876@gmail.com>
2024-06-21 15:19:58 -0700

    multiline printout

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,3 @@
 #!/usr/bin/node
 /*This program prints out multiple lines */
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
