229244877c0051d6158b0ec2051a42bb780cbbe9
Peter Anthony <peterez5876@gmail.com>
2024-06-21 17:07:50 -0700

    Using a for loop to multi-print a string in javascript

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/* a script that prints x times “C is fun”
+ */
+
+const x = process.argv[2];
+
+if (x === undefined || parseInt(x)) {
+  console.log('Missing number of occurences');
+} else {
+  for (let i = 0; i < parseInt(x); i++) {
+    console.log('C is fun');
+  }
+}
